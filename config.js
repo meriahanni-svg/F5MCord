ca704467dd5a88b775cb95723f91750db3076ab0
meriahanni-svg <meria.hanni@gmail.com>
2026-02-06 19:59:01 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,14 @@
+// بيانات مشروع Firebase الخاص بك
+const firebaseConfig = {
+    apiKey: "AIzaSyB6jGiS3japB3t7JMhspY3_D5S_-SAzrHc",
+    authDomain: "f5mcord-103.firebaseapp.com",
+    databaseURL: "https://f5mcord-db-default-rtdb.firebaseio.com",
+    projectId: "f5mcord-103",
+    storageBucket: "f5mcord-103.firebasestorage.app",
+    messagingSenderId: "776499493207",
+    appId: "1:776499493207:web:bc820bb595d709086c6612"
+};
+
+// تهيئة Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
