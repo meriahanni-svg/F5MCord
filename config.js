16efc450f2b404896a029a5313f96ccab085b6e8
meriahanni-svg <meria.hanni@gmail.com>
2026-02-06 20:37:10 +0100

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 const firebaseConfig = {
     apiKey: "AIzaSyB6jGiS3japB3t7JMhspY3_D5S_-SAzrHc",
     authDomain: "f5mcord-103.firebaseapp.com",
-    databaseURL: "https://f5mcord-db-default-rtdb.firebaseio.com",
+    databaseURL: "https://f5mcord-103.firebaseio.com",
     projectId: "f5mcord-103",
     storageBucket: "f5mcord-103.firebasestorage.app",
     messagingSenderId: "776499493207",
